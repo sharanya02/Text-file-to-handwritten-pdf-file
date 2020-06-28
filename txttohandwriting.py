bf44f60cf004aba76f7dfc198add772cb26c2e6f
Sharanya Mukherjee <59661067+sharanya02@users.noreply.github.com>
2020-06-28 12:23:50 +0530

    Update txttohandwriting.py

--- txttohandwriting.py
+++ txttohandwriting.py
@@ -1,5 +1,5 @@
 from PIL import Image
-BG = Image.open("c:/Users/shara/myfont/bg.png")
+BG = Image.open("myfont/bg.png")
 sizeOfSheet =BG.width
 gap, _  = 0,0
 allowedChars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.-?!() 1234567890'
@@ -9,7 +9,7 @@ def writee(char):
         pass
     else:
         char.lower()
-        cases = Image.open("c:/Users/shara/myfont/%s.png"%char)
+        cases = Image.open("myfont/%s.png"%char)
         BG.paste(cases, (gap, _))
         size = cases.width
         gap += size
